7d164acd8db408b574e3aee2d0858e8e4e058516
uNyanda <ayabulelamdlalose@outlook.com>
2024-07-09 17:32:26 +0200

    Jump Search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,36 +2,6 @@
 #include <math.h>
 
 /**
- * linear_search - function searches for a value in an array of integers using
- *		   Linear Search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in the array
- * @value: the value to search for
- *
- * Return: value if present, else -1 if array is NULL or value not present.
- */
-int linear_search(int *array, size_t size, int value)
-{
-	size_t i;
-
-	if (array == NULL)
-	{
-		return (-1);
-	}
-
-	for (i = 0; i < size; i++)
-	{
-
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-		{
-			return (i);
-		}
-	}
-	return (-1);
-}
-
-/**
  * jump_search - Searches for a value in a sorted array using Jump Search
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
@@ -43,7 +13,6 @@ int linear_search(int *array, size_t size, int value)
 int jump_search(int *array, size_t size, int value)
 {
 	size_t step, prev, current;
-	int result;
 
 	if (array == NULL || size == 0)
 	{
@@ -62,13 +31,19 @@ int jump_search(int *array, size_t size, int value)
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", prev, current);
 
-	/* Adjust current if it exceeds array bounds */
-	current = (current < size) ? current : size - 1;
+	/* Adjust current if it exceeds array size */
+	current = (current < size ? current : size - 1);
 
-	/* call linear_search on the identified block */
-	result = linear_search(array + prev, current - prev + 1, value);
-
-	/* Adjust the result to be relative to the whole array */
-	return ((result == -1) ? -1 : (int)(prev + result));
+	/* Linear search in the identified block */
+	while (prev <= current)
+	{
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+		{
+			return (prev);
+		}
+		prev++;
+	}
+	return (-1);
 }
 
