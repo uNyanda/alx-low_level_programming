6bc22de6f32cdcd10a24e1fd8381bc64c9239d22
uNyanda <ayabulelamdlalose@outlook.com>
2024-07-22 14:57:45 +0200

    Jump Search algorithm.

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,7 +17,11 @@ int jump_search(int *array, size_t size, int value)
 	size_t start = 0;
 	size_t end = blocksize, i;
 
-	/* 1, 2, 3, 4, 5, 6, 7, 8, 9 */
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
+
 	printf("Value checked array[%lu] = [%d]\n", start, array[start]);
 
 	while (array[end] < value && end < size)
