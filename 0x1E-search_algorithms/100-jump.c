8ef31c0a8bc8dd50578ea64e9874418b437e8f6e
uNyanda <ayabulelamdlalose@outlook.com>
2024-07-22 15:46:55 +0200

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -33,7 +33,7 @@ int jump_search(int *array, size_t size, int value)
 
 	printf("Value found between indexes [%lu] and [%lu]\n", start, end);
 
-	for (i = start; i < size; i++)
+	for (i = start; i <= end && i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
